31bdbf58582fd0eff34fc046b5faef5a30e119a2
Frojdi Dymylja <frojdi@faulttolerance.net>
2025-08-21 11:55:38 +0200

    checkpint

--- crates/application/src/abci_executor.rs
+++ crates/application/src/abci_executor.rs
@@ -1,3 +1,17 @@
+use tonic::transport::Channel;
+use astro_proto_types::cometbft::abci::v1beta3::abci_client::AbciClient;
+use astro_types::Block;
+
 pub struct CosmosSDKExecutor {
-    client,
+    client: AbciClient<Channel>,
+}
+
+impl CosmosSDKExecutor {
+    pub(super) fn new(client: AbciClient<Channel>) -> Self {
+        Self { client }
+    }
+
+    pub(super) fn execute_block(&self, block: &Block) {
+
+    }
 }
